daddcbfe5696cdfce7d503f83ea76b4b0372382d
Turlough Mullan <duke1199sp@gmail.com>
2022-01-28 12:34:04 +0100

    Starting to build the socket

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,20 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/28 11:37:35 by tmullan       #+#    #+#                 */
-/*   Updated: 2022/01/28 11:37:36 by tmullan       ########   odam.nl         */
+/*   Updated: 2022/01/28 12:32:54 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/socket.h>
+
+int		main(int argc, char *argv[]) {
+	
+	/* Create the Socket
+	socket(domain, type, protocol); */
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0); // Protect against failure
+
+	/* Identify (name) a socket
+	bind(int socket, const struct sockaddr *address, socklen_t address_len); */
+	
+	
+}
