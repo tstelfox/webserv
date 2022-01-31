5d2b5a599708e80c46ee5e3288c2edbd170d98c2
Turlough Mullan <duke1199sp@gmail.com>
2022-01-31 16:43:47 +0100

    Links from Amber for building the server

--- what.sh
+++ what.sh
@@ -2,3 +2,11 @@ echo what
 
 # https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
 
+# https://ncona.com/2019/04/building-a-simple-server-with-cpp/ 
+
+# https://www.tutorialspoint.com/cplusplus/cpp_web_programming.htm
+
+# https://www.lowtek.com/sockets/select.html
+
+# https://www.educba.com/socket-programming-in-c-plus-plus/
+
