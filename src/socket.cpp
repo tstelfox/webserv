2611492b5c4dbe1b70d6cbc76567b95ccd0c0b9d
Turlough Mullan <duke1199sp@gmail.com>
2022-02-03 12:13:54 +0100

    Werkin on it

--- src/socket.cpp
+++ src/socket.cpp
@@ -6,17 +6,37 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/02 19:19:15 by tmullan       #+#    #+#                 */
-/*   Updated: 2022/02/03 11:24:01 by tmullan       ########   odam.nl         */
+/*   Updated: 2022/02/03 12:07:01 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.hpp"
 
-socketMan::socketMan(int domain, int service, int protocol) {
-	_connection = socket(domain, service, protocol);
+socketMan::socketMan(int domain, int service, int protocol, int port,
+		u_long interface) {
+	
+	// Set up address structure
+	address.sin_family = domain;
+	address.sin_addr.s_addr = htonl(interface);
+	address.sin_port = htons(port);
+	// Create the socket
+	sock = socket(domain, service, protocol);
+	// Bind/connect the socket
+	connection = connect_server(sock, address);
+	
+
 }
 
-socketMan::~socketMan() {
+socketMan::~socketMan() {}
+
+void socketMan::test_connection(int to_test) {
+	if (to_test < 0) {
+		std::cerr << "Failed to connect";
+		exit(EXIT_FAILURE);
+	}
+
+}
+
+int	serverSock::connect_server(int sock, struct sockaddr_in address) {
 	
 }
-
--- src/socket.hpp
+++ src/socket.hpp
@@ -6,19 +6,38 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/02 15:17:36 by tmullan       #+#    #+#                 */
-/*   Updated: 2022/02/03 11:23:53 by tmullan       ########   odam.nl         */
+/*   Updated: 2022/02/03 12:13:29 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#prama once
+#pragma once
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
 
 class socketMan
 {
 	public:
-		socketMan(int domain, int service, int protocol);
-		~socketMan();
+		socketMan(int domain, int service, int protocol,
+			int port, u_long interface);
+		virtual ~socketMan(); // This means inherited classes don't need to have a destructor
+
+		virtual int connect_server(int sock, struct sockaddr_in	address) = 0;
+		void		test_connection(int);
 	private:
-		int _connection;
+		int sock;
+		int connection;
+		struct sockaddr_in	address;
 };
 
+class	serverSock : public socketMan {
+	
+	public:
+		serverSock(int domain, int service, int protocol,
+			int port, u_long interface) : socketMan(domain, service, protocol, port, interface);
+
+		virtual int	connect_server(int sock, struct sockaddr_in address);
+	private:
+		serverSock();
+		
+};
