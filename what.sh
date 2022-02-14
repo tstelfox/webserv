780cbd8d93570919d6dd12421e5ca6e71d200bb7
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-02-14 15:01:02 +0100

    Need to sit and think about how this project is ultimately fully divided this week

--- what.sh
+++ what.sh
@@ -1,5 +1,3 @@
-echo what
-
 # https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
 
 # https://ncona.com/2019/04/building-a-simple-server-with-cpp/ 
