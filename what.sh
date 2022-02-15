83ae7e0df85ed12d731e9248712c7a267767da94
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-02-15 13:36:54 +0100

    Another day another small step for webserv

--- what.sh
+++ what.sh
@@ -2,13 +2,17 @@
 
 # https://ncona.com/2019/04/building-a-simple-server-with-cpp/ 
 
-# https://www.tutorialspoint.com/cplusplus/cpp_web_programming.htm
-
 # https://www.lowtek.com/sockets/select.html
 
 # https://www.educba.com/socket-programming-in-c-plus-plus/
 
 # https://www.ibm.com/support/knowledgecenter/ssw_ibm_i_72/rzab6/xnonblock.htm
+
+
+
+# The following is CGI stuff tbd at the end
+
+# https://www.tutorialspoint.com/cplusplus/cpp_web_programming.htm
 
 # http://www.mnuwer.dbasedeveloper.co.uk/dlearn/web/session01.htm
 
