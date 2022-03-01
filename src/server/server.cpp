6c6333b292a2ff1348f9a5e0de9591dfd1f57f0d
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-03-01 16:21:23 +0100

    Working on the basics of poll()

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 18:59:58 by tmullan       #+#    #+#                 */
-/*   Updated: 2022/02/22 19:16:22 by tmullan       ########   odam.nl         */
+/*   Updated: 2022/03/01 16:20:57 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,80 @@ serverBoy::~serverBoy() {}
 
 void	serverBoy::runServer(int backlog) {
 	
+	/* The following is an attempt at using poll() */
+
+	struct pollfd poll_set[42]; // 42 is arbitrary I believe
+	int numfds = 0;
+	// int max_fd = 0;
+	int socket_fd = _socket->getSock();
+	memset(poll_set, 0, sizeof(poll_set));
+	poll_set[0].fd = socket_fd;
+	poll_set[0].events = POLLIN;
+	int timeout = (30 * 1000);
+	numfds++;
+	// update_maxfd(socket_fd, &max_fd);
+	int new_fd = -1;
+
+	int ret;
+	std::cout << "Socket fd is: " << socket_fd << std::endl;
+	// int yonked = -1;
+
+	// do {
+	// }
+	// all of this needs to be filtered through poll() 
+	
 	_socket->listenServer(backlog);
+	
 	while (true) {
-		socklen_t	addrlen;
-		ready_socket = accept(_socket->getSock(), (struct sockaddr *)&_socket->getAddr(), (socklen_t *)&addrlen);
+		std::cout << "Waiting on poll()..." << std::endl;
+		ret = poll(poll_set, numfds, timeout);
+		if (ret < 0) {
+			std::cout << "Shit's fucked" << std::endl;
+			break;
+		}
+		if (ret == 0) {
+			std::cout << "Poll timed out. End" << std::endl;
+			break;
+		}
+		for (int i = 0; i < numfds; i++) {
+			if (poll_set[i].revents == 0)
+				continue;
+			if (poll_set[i].revents != POLLIN) {
+				std::cout << "Error. revents = " << poll_set[i].revents << std::endl;
+				break;
+			}
+			if (poll_set[i].fd == socket_fd) {
+				std::cout << "Listening socket is readable" << std::endl;
+				do {
+					// std::cout << "Is this gonna loop?" << std::endl;
+					new_fd = accept(socket_fd, NULL, NULL);
+					if (new_fd < 0) {
+						if (errno != EWOULDBLOCK) {
+							std::cout << "Accept balls" << std::endl;
+							// yonked = 1;
+						}
+						break;
+					}
+					std::cout << "New incoming connection " << new_fd << std::endl;
+					poll_set[numfds].fd = new_fd;
+					poll_set[numfds].events = POLLIN;
+					numfds++;
+				} while (new_fd != -1);
+			}
+			std::cout << "Well?" << std::endl;
+			// else {
+
+			// }
+		}
+		// if (yonked == 1)
+		// 	break;
+		// socklen_t	addrlen;
+		// ready_socket = accept(_socket->getSock(), (struct sockaddr *)&_socket->getAddr(), (socklen_t *)&addrlen);
 		
 		
 		char buffer[1024] = {0};
-		int valread = read(ready_socket, buffer, 1024);
+		int valread = recv(poll_set[0].fd, buffer, sizeof(buffer), 0);
+		// int valread = read(ready_socket, buffer, 1024);
 		std::cout << buffer << std::endl;
 		// Parse the buffer for GET/POST/DELETE
 		// Build header
--- src/webserv.hpp
+++ src/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 19:01:03 by tmullan       #+#    #+#                 */
-/*   Updated: 2022/02/04 19:18:36 by tmullan       ########   odam.nl         */
+/*   Updated: 2022/03/01 14:38:40 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <sstream>
 #include <fstream>
+#include <poll.h>
 
 #include "socket.hpp"
 #include "server.hpp"
