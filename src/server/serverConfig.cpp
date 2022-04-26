e52d293a20ca90e2522c11a9a14aaf3dd173d08b
Turlough Stelfox Mullan <duke1199sp@gmail.com>
2022-04-26 13:21:42 +0200

    Clion formatter used on serverConfig

--- src/server/serverConfig.cpp
+++ src/server/serverConfig.cpp
@@ -14,21 +14,17 @@
 #include "serverConfig.hpp"
 #include <string>
 
-WSERV::serverConfig::serverConfig()
-{
+WSERV::serverConfig::serverConfig() {
 }
 
-WSERV::serverConfig::~serverConfig()
-{
+WSERV::serverConfig::~serverConfig() {
 }
 
-WSERV::serverConfig::serverConfig(serverConfig const & copy)
-{
+WSERV::serverConfig::serverConfig(serverConfig const &copy) {
     *this = copy;
 }
 
-WSERV::serverConfig & WSERV::serverConfig::operator = (serverConfig const & copy)
-{
+WSERV::serverConfig &WSERV::serverConfig::operator=(serverConfig const &copy) {
     this->_port = copy._port;
     this->_host = copy._host;
     this->_server_name = copy._server_name;
@@ -42,82 +38,66 @@ WSERV::serverConfig & WSERV::serverConfig::operator = (serverConfig const & copy
 
 /* Getters & Setters*/
 
-int WSERV::serverConfig::get_port( void ) const
-{
+int WSERV::serverConfig::get_port() const {
     return (this->_port);
 }
 
-void WSERV::serverConfig::set_port( int const data )
-{
+void WSERV::serverConfig::set_port(int const data) {
     this->_port = data;
 }
 
-std::string WSERV::serverConfig::get_host( void ) const
-{
+std::string WSERV::serverConfig::get_host() const {
     return (this->_host);
 }
 
-void WSERV::serverConfig::set_host( std::string const data )
-{
+void WSERV::serverConfig::set_host(std::string const &data) {
     this->_host = data;
 }
 
-std::string WSERV::serverConfig::get_server_name( void ) const
-{
+std::string WSERV::serverConfig::get_server_name() const {
     return (this->_server_name);
 }
 
-void WSERV::serverConfig::set_server_name( std::string const data )
-{
+void WSERV::serverConfig::set_server_name(std::string const data) {
     this->_server_name = data;
 }
 
-int WSERV::serverConfig::get_maxfilesize( void ) const
-{
+int WSERV::serverConfig::get_maxfilesize() const {
     return (this->_maxfilesize);
 }
 
-void WSERV::serverConfig::set_maxfilesize( int const data )
-{
+void WSERV::serverConfig::set_maxfilesize(int const data) {
     this->_maxfilesize = data;
 }
 
-std::string WSERV::serverConfig::get_error_page( void ) const
-{
+std::string WSERV::serverConfig::get_error_page() const {
     return (this->_error_page);
 }
 
-void WSERV::serverConfig::set_error_page( std::string const data )
-{
+void WSERV::serverConfig::set_error_page(std::string const data) {
     this->_error_page = data;
 }
 
-std::string WSERV::serverConfig::get_cgi_file_types( void ) const
-{
+std::string WSERV::serverConfig::get_cgi_file_types() const {
     return (this->_cgi_file_types);
 }
 
-void WSERV::serverConfig::set_cgi_file_types( std::string const data )
-{
+void WSERV::serverConfig::set_cgi_file_types(std::string const data) {
     this->_cgi_file_types = data;
 }
 
-int WSERV::serverConfig::get_time_out( void ) const
-{
+int WSERV::serverConfig::get_time_out() const {
     return (this->_time_out);
 }
 
-void WSERV::serverConfig::set_time_out( int const data )
-{
+void WSERV::serverConfig::set_time_out(int const data) {
     this->_time_out = data;
 }
 
-std::vector<WSERV::Location> WSERV::serverConfig::get_Location_vec( void ) const
-{
+std::vector<WSERV::Location> WSERV::serverConfig::get_Location_vec() const {
     return (this->_Location_vec);
 }
 
-void WSERV::serverConfig::set_Location_vec( std::vector<WSERV::Location> const data )
-{
+void WSERV::serverConfig::set_Location_vec(std::vector<WSERV::Location> const data) {
     this->_Location_vec = data;
 }
--- src/server/serverConfig.hpp
+++ src/server/serverConfig.hpp
@@ -20,43 +20,60 @@
 # include <set>
 # include <vector>
 
-namespace WSERV
-{
-    class serverConfig
-    {
+namespace WSERV {
+    class serverConfig {
     private:
-        int			_port; // std::set<int>        _port;
+        int _port; // std::set<int>        _port;
         std::string _host;
         std::string _server_name;
-        int         _maxfilesize;
+        int _maxfilesize;
         std::string _error_page;
         std::string _cgi_file_types;
-        int         _time_out;
+        int _time_out;
         std::vector<WSERV::Location> _Location_vec;
 
     public:
         serverConfig();
+
         ~serverConfig();
-        serverConfig(serverConfig const & copy);
-        serverConfig & operator = (serverConfig const & copy);
+
+        serverConfig(serverConfig const &copy);
+
+        serverConfig &operator=(serverConfig const &copy);
+
         friend class Parser;
 
-        int 		get_port( void ) const;
-        std::string get_host( void ) const;
-        std::string get_server_name( void ) const;
-        int 		get_maxfilesize( void ) const;
-        std::string get_error_page( void ) const;
-        std::string get_cgi_file_types( void ) const;
-        int 		get_time_out( void ) const;
-        std::vector<WSERV::Location> 	get_Location_vec( void ) const;
-        void set_port( int const data );
-        void set_host( std::string const data );
-        void set_server_name( std::string const data );
-        void set_Location_vec(std::vector<WSERV::Location> const data );
-        void set_maxfilesize( int const data );
-        void set_time_out( int const data );
-        void set_error_page( std::string const data );
-        void set_cgi_file_types( std::string const data );
+        int get_port() const;
+
+        std::string get_host() const;
+
+        std::string get_server_name() const;
+
+        int get_maxfilesize() const;
+
+        std::string get_error_page() const;
+
+        std::string get_cgi_file_types() const;
+
+        int get_time_out() const;
+
+        std::vector<WSERV::Location> get_Location_vec() const;
+
+        void set_port(int const data);
+
+        void set_host(std::string const &data);
+
+        void set_server_name(std::string const data);
+
+        void set_Location_vec(std::vector<WSERV::Location> const data);
+
+        void set_maxfilesize(int const data);
+
+        void set_time_out(int const data);
+
+        void set_error_page(std::string const data);
+
+        void set_cgi_file_types(std::string const data);
     };
 }
 
